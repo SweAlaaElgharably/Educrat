1bb579e6db02b0dfda73ea6aab157b01fbd175af
SweAlaaElgharably <swe.alaaElgharably@gmail.com>
2025-11-28 16:41:20 +0200

    dockerize project

--- educrat/settings.py
+++ educrat/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*+k7sbdrq^5ig@y@fhjgp-p&bs-t#sx3$ua40tg_i9_be=(kd9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == True
 
-ALLOWED_HOSTS = ['alaaelgharably248.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['alaaelgharably248.pythonanywhere.com', '127.0.0.1', 'localhost', 'http://api.cr-ai.cloud']
 
 
 # Application definition
@@ -177,7 +179,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    "https://crai-ksa.netlify.app"
+    "https://crai-ksa.netlify.app",
+    "http://cr-ai.cloud"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -189,8 +192,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alaaelgharably1@gmail.com'
-EMAIL_HOST_PASSWORD = 'lmos nsjm fbau yfsn'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER") 
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD") 
 DEFAULT_FROM_EMAIL = 'admin@crai.com'
 
 
