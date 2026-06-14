b0ff94c427c30a53ff690aae1e0b3706da7d611e
SweAlaaElgharably <swe.alaaelgharably@gmail.com>
2026-06-14 03:44:42 +0300

    update user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -10,7 +10,7 @@ class UserCreateSerializer(BaseUserCreateSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'phone', 'bio', 'area_of_expertise', 'picture', 'username', 'id_card_face', 'id_card_back')
+        fields = ('id', 'first_name', 'last_name', 'email', 'phone', 'bio', 'area_of_expertise', 'picture', 'is_influencer', 'is_verified', 'username', 'id_card_face', 'id_card_back')
 
 class AdminUserSerializer(serializers.ModelSerializer):
     class Meta:
